be722b9440bae9a2605691c02027ebef1eaad4ca
Sonu Kumar <sonu91428dingh>
2024-02-12 15:05:03 +0530

    Home,LoginPage and SignupPage

---
+++ src/App.js
@@ -0,0 +1,42 @@
+// import React from 'react';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import SignupPage from './pages/SignupPage';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+// import ProductList from './features/product-list/ProductList';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+
+  {
+    path: "/LoginPage",
+    element: <LoginPage/>,
+  },
+  {
+    path:"/SignupPage",
+    element: <SignupPage/>,
+  },
+]);
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <RouterProvider router={router} />
+      
+    </div>
+  );
+}
+
+export default App;
